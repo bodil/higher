305a04f3d8ef3943ebbbd66cf2b280a4fb09a3b5
bodil <bodil@users.noreply.github.com>
2023-01-20 22:17:21 +0000

    deploy: 18775e4148b500232d27d62cd69de69af497226e

--- implementors/higher/bifunctor/trait.Bifunctor.js
+++ implementors/higher/bifunctor/trait.Bifunctor.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "higher":[],
-"higher_effect":[["impl&lt;'a, A:&nbsp;'a, B:&nbsp;'a&gt; <a class=\"trait\" href=\"higher/bifunctor/trait.Bifunctor.html\" title=\"trait higher::bifunctor::Bifunctor\">Bifunctor</a>&lt;'a, A, B&gt; for <a class=\"struct\" href=\"higher_effect/struct.IO.html\" title=\"struct higher_effect::IO\">IO</a>&lt;'a, A, B&gt;"]]
+"higher_effect":[["impl&lt;'a, A:&nbsp;'a, E:&nbsp;'a&gt; <a class=\"trait\" href=\"higher/bifunctor/trait.Bifunctor.html\" title=\"trait higher::bifunctor::Bifunctor\">Bifunctor</a>&lt;'a, A, E&gt; for <a class=\"struct\" href=\"higher_effect/struct.IO.html\" title=\"struct higher_effect::IO\">IO</a>&lt;'a, A, E&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/higher/error/trait.ApplicativeError.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"higher":[],
+"higher_effect":[["impl&lt;'a, A:&nbsp;'a, E:&nbsp;'a&gt; <a class=\"trait\" href=\"higher/error/trait.ApplicativeError.html\" title=\"trait higher::error::ApplicativeError\">ApplicativeError</a>&lt;'a, A, E&gt; for <a class=\"struct\" href=\"higher_effect/struct.IO.html\" title=\"struct higher_effect::IO\">IO</a>&lt;'a, A, E&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
