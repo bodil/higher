64bc195bca9b79c25917b249c0b269855b240e51
Bodil Stokke <17880+bodil@users.noreply.github.com>
2023-01-27 10:34:12 +0000

    oops

--- effect/src/lib.rs
+++ effect/src/lib.rs
@@ -3,14 +3,9 @@
 #![warn(unreachable_pub, missing_debug_implementations)]
 
 pub mod effect;
-use higher::Functor;
-
 #[doc(inline)]
 pub use crate::effect::Effect;
 
 pub mod io;
 #[doc(inline)]
 pub use crate::io::IO;
-
-#[derive(Functor)]
-struct Welp<A>(A);
