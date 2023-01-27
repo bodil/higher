25cb928a8b0c687c6886d0e8ae8533aebb3d4f6b
Bodil Stokke <17880+bodil@users.noreply.github.com>
2023-01-27 16:14:17 +0000

    Some top level docs for the effect crate.

--- effect/src/lib.rs
+++ effect/src/lib.rs
@@ -2,6 +2,12 @@
 #![forbid(rust_2018_idioms)]
 #![warn(unreachable_pub, missing_debug_implementations)]
 
+//! Effect monads for [`higher`](https://docs.rs/higher).
+//!
+//! These are of limited usefulness, being single-threaded out of necessity
+//! because of our inability to specify [`Send`](Send) bounds, but they serve as
+//! a good example of effects with monadic abstractions.
+
 pub mod effect;
 #[doc(inline)]
 pub use crate::effect::Effect;
